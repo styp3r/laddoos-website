1f8e7be24d01eac1b346b6bb2ba5f5aef3af7ae0
Navjyot Jaiswal <stypertypes@gmail.com>
2021-09-08 13:01:40 +0530

    add shopping cart icon and move navbar items to the right.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,6 @@ import Logo from '../images/laddoosLogo1.jpg';
 
 function Navbar(){
     return (<div>
-        <div className = "order">ORDER ONLINE OR CALL US +91 99452 58005
-        </div>
-        <hr/>
         <div className = "navbar">
             <div className = "navbarContainer">
                 <div className = "logoContainer">
@@ -14,11 +11,13 @@ function Navbar(){
                 <div className = "navItemsContainer">
                     <div className = "navItems">
                         <a id = "home" href = "#">Home</a>
-                        <a id = "products" href = "#">Products</a>
+                        <a id = "products" href = "#">Menu</a>
+                        <a id = "products" href = "#">Order Online</a>
                         <a id = "about" href = "#">About Us</a>
                         <a id = "contact" href = "#">Contact Us</a>
                     </div>
                 </div>
+                <a href = "#"><i class="fas fa-shopping-cart"></i></a>
             </div>
         </div>
         </div>
