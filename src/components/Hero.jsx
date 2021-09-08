e63c8ac43847b069250bf287f056d2b1fed317ba
Navjyot Jaiswal <stypertypes@gmail.com>
2021-09-08 14:47:19 +0530

    change hero image every 5s with transition effect.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Sweet7 from '../images/sweet7.jpg';
+import Sweet8 from '../images/sweet8.jpg';
 import Button from './Button.jsx';
 
+
+
 function Hero(){
+
+
+    const [banner, updateBanner] = useState(Sweet7);
+    const [animate, setAnimate] = useState("");
+
+
+    function changeHero(){
+
+        const interval = setInterval(function(){ 
+            if(banner == Sweet7){
+                setAnimate("heroImageTransition");
+                updateBanner(Sweet8);
+                clearInterval(interval);
+            } else if(banner == Sweet8){
+                setAnimate("heroImageTransition1");
+                updateBanner(Sweet7);
+                clearInterval(interval); 
+            }
+         }, 5000);
+         return banner;
+    }
+
+
     return (
         <div className = "heroContent">
             <div>
                 <h1 className = "heroText">Taste the goodness at Laddoos.</h1>
                 <Button cName = "heroButton" buttonText = "Explore Menu"/>
             </div>
-            <img src = {Sweet7} width = "100%" height = "100%"/>
+            <img className = {animate} src = {changeHero()} width = "100%" height = "100%"/>
         </div>
     );
 }
