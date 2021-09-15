9c07a1943191fc805d542c2882f2158171544163
Navjyot Jaiswal <stypertypes@gmail.com>
2021-09-15 17:02:57 +0530

    Add more items in OnlineOrder page.

--- src/components/OnlineOrder.jsx
+++ src/components/OnlineOrder.jsx
@@ -11,6 +11,9 @@ function OnlineOrder(){
             <ItemBuy name = {products[0].name} title = {products[0].title} img = {products[0].img} price = {products[0].price}/>
             <ItemBuy name = {products[1].name} title = {products[1].title} img = {products[1].img} price = {products[1].price}/>
             <ItemBuy name = {products[2].name} title = {products[2].title} img = {products[2].img} price = {products[2].price}/>
+            <ItemBuy name = {products[3].name} title = {products[3].title} img = {products[3].img} price = {products[3].price}/>
+            <ItemBuy name = {products[4].name} title = {products[4].title} img = {products[4].img} price = {products[4].price}/>
+            <ItemBuy name = {products[5].name} title = {products[5].title} img = {products[5].img} price = {products[5].price}/>
         </div>);
 }
 
--- src/products.js
+++ src/products.js
@@ -24,6 +24,30 @@ const products = [
         img: "https://bit.ly/3h4NOw9",
         price: "Rs. 100"
 
+    },
+    {
+        id: 3,
+        name: "four",
+        title: "Rasagulla3",
+        img: "https://bit.ly/3h4NOw9",
+        price: "Rs. 100"
+
+    },
+    {
+        id: 4,
+        name: "five",
+        title: "Rasagulla4",
+        img: "https://bit.ly/3h4NOw9",
+        price: "Rs. 100"
+
+    },
+    {
+        id: 5,
+        name: "six",
+        title: "Rasagulla5",
+        img: "https://bit.ly/3h4NOw9",
+        price: "Rs. 100"
+
     }
 ];
 
