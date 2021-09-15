cfbc93cea605d6cd4e3bb717bedde1eb01a330ec
Navjyot Jaiswal <stypertypes@gmail.com>
2021-09-16 00:04:07 +0530

    Link the first item box in Menu page with OnlineOrder page.

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Item from './Item.jsx';
 import MenuProducts from '../MenuProducts.js';
+import {Link} from 'react-router-dom';
 
 function Products(){
 
@@ -8,12 +9,13 @@ function Products(){
 
     return (
         <div className = "itemListRight">
-            <Item title = {MenuProducts[0].title} img = {MenuProducts[0].img} />
+            <Link id = "allProducts" to="/onlineOrder"><Item title = {MenuProducts[0].title} img = {MenuProducts[0].img} /></Link>
             <Item title = {MenuProducts[1].title} img = {MenuProducts[1].img} />
             <Item title = {MenuProducts[2].title} img = {MenuProducts[2].img} />
-            <Item title = {MenuProducts[0].title} img = {MenuProducts[0].img} />
-            <Item title = {MenuProducts[1].title} img = {MenuProducts[1].img} />
-            <Item title = {MenuProducts[2].title} img = {MenuProducts[2].img} />
+            <Item title = {MenuProducts[3].title} img = {MenuProducts[3].img} />
+            <Item title = {MenuProducts[4].title} img = {MenuProducts[4].img} />
+            <Item title = {MenuProducts[5].title} img = {MenuProducts[5].img} />
+            <Item title = {MenuProducts[6].title} img = {MenuProducts[6].img} />
         </div>);
 }
 
